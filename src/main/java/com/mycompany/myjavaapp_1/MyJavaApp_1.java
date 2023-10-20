9cc3118d03d6d95b422750b928d770c699d3a1a8
tonathaly <nathalyalbino.029@gmail.com>
2023-10-20 21:19:06 +0800

    second commit

--- src/main/java/com/mycompany/myjavaapp_1/MyJavaApp_1.java
+++ src/main/java/com/mycompany/myjavaapp_1/MyJavaApp_1.java
@@ -3,6 +3,6 @@ package com.mycompany.myjavaapp_1;
 public class MyJavaApp_1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World from Java!");
     }
 }
